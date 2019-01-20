7e35c292d9ff8276e4d3d128e8413a08d7af3388
brnkgabriel <brnkgabriel@gmail.com>
2019-01-20 21:48:55 +0100

    frameworked each page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,7 +8,7 @@ export default {
         { icon: 'fa fa-dashboard', text: 'Dashboard', route: '/' },
         { icon: 'fa fa-pencil', text: 'Quiz', route: '/quiz' },
         { icon: 'fa fa-money', text: 'Rank', route: '/rank' },
-        { icon: 'fa fa-paperclip', text: 'Material', route: '/material' }
+        { icon: 'fa fa-paperclip', text: 'Materials', route: '/materials' }
       ]
     }
   }
